b5fcf5caf0f6d1b32dce5eda7a874e2233ac6493
mohammad <mohammadsheikhian70@gmail.com>
2018-07-23 19:23:43 +0430

    Email controllers has been added, #2

---
+++ panda/controllers/emails.py
@@ -0,0 +1,47 @@
+import itsdangerous
+from nanohttp import json, context, validate, settings, HTTPStatus
+from restfulpy.controllers import ModelRestController
+from restfulpy.orm import DBSession, commit
+
+from panda.models import Member, RegisterEmail
+
+
+class EmailsController(ModelRestController):
+
+    @commit
+    @json
+    @validate(
+        email=dict(
+            required=(True, '701 Invalid format email'),
+            pattern=(
+                r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)',
+                '701 Invalid format email'
+            )
+        )
+    )
+    def claim(self):
+        email = context.form.get('email')
+
+        if DBSession.query(Member.email).filter(Member.email == email).count():
+            raise HTTPStatus(
+                '601 The requested email address is already registered.'
+            )
+
+        serializer = \
+            itsdangerous.URLSafeTimedSerializer(settings.activation.secret)
+
+        token = serializer.dumps(email)
+
+        DBSession.add(
+            RegisterEmail(
+                to=email,
+                subject='Register your CAS account',
+                body={
+                    'register_token': token,
+                    'register_url': settings.activation.url
+                }
+            )
+        )
+
+        return dict(email=email)
+
