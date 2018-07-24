7725df5fa4eac90b111bc11d2ef894db03accead
mohammad <mohammadsheikhian70@gmail.com>
2018-07-24 19:20:13 +0430

    Title validator has been added, #16

--- panda/validators.py
+++ panda/validators.py
@@ -1,4 +1,9 @@
+import re
+
 from nanohttp import validate
+
+
+user_title_pattern = re.compile('^[a-zA-Z][\w]{5,16}$')
 
 
 email_validator = validate(
@@ -8,6 +13,14 @@ email_validator = validate(
             '(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)',
             '701 Invalid email format'
         )
+    )
+)
+
+
+title_validator = validate(
+    title=dict(
+        required=(True, '705 Invalid title format'),
+        pattern=(user_title_pattern, '705 Invalid title format')
     )
 )
 
