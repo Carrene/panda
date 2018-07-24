f7c65ed80b8696d9ff08891aa5b22d556502405d
mohammad <mohammadsheikhian70@gmail.com>
2018-07-24 16:54:46 +0430

    Availabilitie controller has been created, #15

--- panda/controllers/root.py
+++ panda/controllers/root.py
@@ -2,12 +2,13 @@ from nanohttp import Controller, json
 from restfulpy.controllers import RootController
 
 import panda
+from .availabilities import AvailabilitiesController
 from .emails import EmailsController
 
 
 class ApiV1(Controller):
-
     emails = EmailsController()
+    availabilities = AvailabilitiesController()
 
     @json
     def version(self):
