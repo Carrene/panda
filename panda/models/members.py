7f1fe113009d471d9129eeaa4ef211dbf082b46a
mohammad <mohammadsheikhian70@gmail.com>
2018-07-25 17:17:00 +0430

    Adding password field to member model

--- panda/models/members.py
+++ panda/models/members.py
@@ -1,13 +1,51 @@
+import os
+from hashlib import sha256
+
 from restfulpy.orm import DeclarativeBase, Field
+from restfulpy.principal import JwtPrincipal
 from sqlalchemy import Unicode, Integer
+from sqlalchemy.orm import synonym
 
 
 class Member(DeclarativeBase):
     __tablename__ = 'member'
 
     id = Field(Integer, primary_key=True)
-
     email = Field(Unicode(100), unique=True, index=True)
-
     title = Field(Unicode(100))
+    _password = Field('password', Unicode(128), index=True, protected=True)
+
+    def _hash_password(cls, password):
+        salt = sha256()
+        salt.update(os.urandom(60))
+        salt = salt.hexdigest()
+
+        hashed_pass = sha256()
+        # Make sure password is a str because we cannot hash unicode objects
+        hashed_pass.update((password + salt).encode('utf-8'))
+        hashed_pass = hashed_pass.hexdigest()
+
+        password = salt + hashed_pass
+        return password
+
+    def _set_password(self, password):
+        """Hash ``password`` on the fly and store its hashed version."""
+        self._password = self._hash_password(password)
+
+    def _get_password(self):
+        """Return the hashed version of the password."""
+        return self._password
+
+    password = synonym(
+        '_password',
+        descriptor=property(_get_password, _set_password),
+        info=dict(protected=True)
+    )
+
+    def create_jwt_principal(self):
+        return JwtPrincipal(dict(
+            id=self.id,
+            email=self.email,
+            name=self.title
+        ))
 
