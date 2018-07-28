c1a1d61b771e184790f9141e7d67ede70c0b0888
mohammad <mohammadsheikhian70@gmail.com>
2018-07-28 18:12:50 +0430

    Mockup data has been removed, #26

--- panda/tests/helpers.py
+++ panda/tests/helpers.py
@@ -7,14 +7,3 @@ from panda.models import Member
 class LoadApplicationTestCase(ApplicableTestCase):
     __application_factory__ = Panda
 
-    @classmethod
-    def mockup(cls):
-        member = Member(
-            email='already.added@example.com',
-            title='username',
-            password='123abcABC'
-        )
-        session = cls.create_session()
-        session.add(member)
-        session.commit()
-
