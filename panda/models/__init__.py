b2f39a571f336fe69425f842e202bcbaa0daacb0
mohammad <mohammadsheikhian70@gmail.com>
2018-07-31 17:30:38 +0430

    Creating client model, #31

--- panda/models/__init__.py
+++ panda/models/__init__.py
@@ -1,3 +1,4 @@
 from .member import Member
+from .client import Client
 from .messaging import RegisterEmail, ResetPasswordEmail
 
---
+++ panda/models/client.py
@@ -0,0 +1,26 @@
+import base64
+
+from restfulpy.orm import DeclarativeBase, Field, relationship
+from sqlalchemy import Unicode, Integer, Binary, ForeignKey
+
+
+class Client(DeclarativeBase):
+    __tablename__ = 'client'
+
+    id = Field(Integer, primary_key=True)
+    title = Field(Unicode(100))
+    redirect_uri = Field(Unicode(100))
+    secret = Field(Binary(32))
+    member_id = Field(Integer, ForeignKey('member.id'))
+
+    member = relationship('Member', back_populates='clients', protected=True)
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            title=self.title,
+            redirect_uri=self.redirect_uri,
+            secret=base64.encodebytes(self.secret),
+            member_id=self.member_id
+        )
+
