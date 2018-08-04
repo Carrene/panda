6c0f8c48a81a87006fca5319328e26c9451b7a15
mohammad <mohammadsheikhian70@gmail.com>
2018-08-04 17:54:23 +0430

    Authorization code story has been done, closes #32

--- panda/controllers/authorization_code.py
+++ panda/controllers/authorization_code.py
@@ -1,12 +1,53 @@
-from nanohttp import json
-from restfulpy.orm import commit
+import itsdangerous
+from nanohttp import json, context, HTTPFound, HTTPStatus, settings, validate
+from restfulpy.authorization import authorize
+from restfulpy.controllers import RestController
+from restfulpy.orm import DBSession
+
+from panda.models import Client
 
 
 class AuthorizationCodeController(RestController):
 
+    @authorize
+    @validate(
+        client_id=dict(required=(True, '605 We don\'t recognize this client')),
+        scope=dict(required=(True, '606 Invalid scope'))
+    )
     @json
-    @commit
-    def get(self, client_id, scope, state, redirect_uri):
+    def create(self):
+        # FIXME Temporarily set here!!!
+        scopes = ['profile', 'email']
 
-        return dict()
+        scope = context.query.get('scope')
+        state = context.query.get('state')
+        redirect_uri = context.query.get('redirect_uri')
+
+        # FIXME: This validation must be performed inside the validation
+        # decorator.
+        if context.form.keys():
+            raise HTTPStatus('707 Form not allowed')
+
+        if scope not in scopes:
+            raise HTTPStatus('606 Invalid scope')
+
+        client = DBSession.query(Client).\
+            filter(Client.id == context.query.get('client_id')).one_or_none()
+        if not client:
+            raise HTTPStatus('605 We don\'t recognize this client')
+
+        if not redirect_uri:
+            redirect_uri = client.redirect_uri
+
+        serializer = itsdangerous.URLSafeTimedSerializer \
+            (settings.authorization_code.secret)
+        code = serializer.dumps(dict(scope=scope))
+
+        location = f'{redirect_uri}?clint_id={client.id}&' \
+            f'scope={scope}&code={code}'
+
+        if state:
+            location = f'{location}&state={state}'
+
+        raise HTTPFound(location)
 
--- panda/tests/test_authorization_code.py
+++ panda/tests/test_authorization_code.py
@@ -1,28 +1,106 @@
-from bddrest.authoring import Update, Remove, when, status
+import os
+from urllib.parse import urlparse, parse_qs
 
-from panda.models import Member
+from bddrest.authoring import Update, Remove, when, status, response
+
+from panda.models import Member, Client
 from panda.tests.helpers import LocadApplicationTestCase
 
 
-class AuthorizationCodePassword(LocadApplicationTestCase):
+class TestAuthorizationCode(LocadApplicationTestCase):
 
     @classmethod
     def mockup(cls):
-        member = Member(
+        session = cls.create_session()
+
+        cls.member = Member(
             email='already.added@example.com',
             title='username',
             password='123abcABC'
         )
-        session = cls.create_session()
-        session.add(member)
+        session.add(cls.member)
+        session.flush()
+
+        cls.client = Client(
+            title='oauth',
+            redirect_uri='http://example.com/oauth2',
+            secret=os.urandom(32),
+            member_id=cls.member.id
+        )
+        session.add(cls.client)
         session.commit()
 
-    def test_get_authorization_code(self):
-        with self.given(
-            'Get authorization code',
-            '/apiv1/authorizationcodes',
-            'GET',
-        ):
-            assert status == 200
+    def test_create_authorization_code(self):
 
+        self.login(
+            email=self.member.email,
+            password='123abcABC',
+            url='/apiv1/tokens',
+            verb='CREATE'
+        )
+
+        with self.given(
+            'Create authorization code',
+            '/apiv1/authorizationcodes',
+            'CREATE',
+            query=dict(
+                client_id=self.client.id,
+                scope='profile',
+                state='123456',
+                redirect_uri='http://example.com/oauth2'
+            )
+        ):
+            assert status == 302
+
+            url_parse = urlparse(response.headers['location'])
+            url_query_string = {
+                k: v[0] for k, v in parse_qs(url_parse.query).items()
+            }
+
+            assert url_query_string['state'] == '123456'
+            assert url_query_string['scope'] == 'profile'
+
+            # Related to the state test case
+            when(
+                'Trying pass to without clint_id parameter',
+                query=Remove('state')
+            )
+            assert status == 302
+            assert 'state'not in url_query_string['state']
+
+            # Related to the client id test case
+            when(
+                'Tring to pass not exist client',
+                query=Update(client_id='1000')
+            )
+            assert status == '605 We don\'t recognize this client'
+
+            when(
+                'Trying pass to without clint_id parameter',
+                query=Remove('client_id')
+            )
+            assert status == '605 We don\'t recognize this client'
+
+            # Related to the scope test case
+            when(
+                'Trying to pass invalid scope name',
+                query=Update(scope='profiles')
+            )
+            assert status == '606 Invalid scope'
+
+            when(
+                'Trying pass to without scope parameter',
+                query=Remove('scope')
+            )
+            assert status == '606 Invalid scope'
+
+            # related to the form parameters
+            when('Trying to pass with form parameters', form=dict(a='a'))
+            assert status == '707 Form not allowed'
+
+            when(
+                'An authorization member trying to create authorization code',
+                authorization=None
+            )
+            assert status == 401
 
