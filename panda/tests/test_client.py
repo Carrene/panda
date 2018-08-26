4eb437b2fd2ec59f08321134b736a1f51e0af70d
Mohammad Sheikhian <39209592+mohammadsheikhian@users.noreply.github.com>
2018-08-26 15:27:37 +0430

    Writing the new test case for metadata verb (#62)
    * Writting the new test case for metadata verb, closes #61
    
    * Renaming method name

--- panda/tests/test_client.py
+++ panda/tests/test_client.py
@@ -90,3 +90,7 @@ class TestClient(LocalApplicationTestCase):
         ):
             assert status == 401
 
+    def test_metadata(self):
+        with self.given('Test metadata verb', '/apiv1/clients', 'METADATA'):
+            assert status == 200
+
