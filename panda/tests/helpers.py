0216d4d6d15a9b01ff1b70c352b83fa191d27901
mohammad <mohammadsheikhian70@gmail.com>
2018-09-10 17:38:45 +0430

    The document path in added to the helper file

--- panda/tests/helpers.py
+++ panda/tests/helpers.py
@@ -1,10 +1,18 @@
+from os import path
+
 from restfulpy.testing import ApplicableTestCase
 
 from panda import Panda, cryptohelpers
 
 
+HERE = path.abspath(path.dirname(__file__))
+DATA_DIRECTORY = path.abspath(path.join(HERE, '../../data'))
+
+
 class LocalApplicationTestCase(ApplicableTestCase):
     __application_factory__ = Panda
+    __story_directory__ = path.join(DATA_DIRECTORY, 'stories')
+    __api_documentation_directory__ = path.join(DATA_DIRECTORY, 'markdown')
 
     def login(self, email, password, url='/apiv1/tokens', verb='CREATE'):
         super().login(
