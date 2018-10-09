941c9d98d6365750f22db2296d1faa542acf4013
mohammad <mohammadsheikhian70@gmail.com>
2018-10-09 12:22:23 +0330

    The phone controllers is implemented, closes #97

--- panda/controllers/phones.py
+++ panda/controllers/phones.py
@@ -1,22 +1,30 @@
 from nanohttp import context, json, RestController, HTTPStatus
+from restfulpy.authorization import authorize
 from restfulpy.orm import commit, DBSession
 
 from ..models import Member
+from ..tokens import PhoneNumberActivationToken
 from ..validators import phone_number_validator
 
 
 class PhoneNumberActivationTokenController(RestController):
 
+    @authorize
     @phone_number_validator
     @json
     @commit
     def create(self):
         phone = context.form['phoneNumber']
+        current_member = Member.current()
+        if current_member.phone is not None:
+            raise HTTPStatus('615 Member Has The Phone Number')
+
         member = DBSession.query(Member) \
             .filter(Member.phone == phone) \
             .one_or_none()
-        if member is None:
-            raise HTTPStatus('616 Phone Already Exists')
+        if member is not None:
+            raise HTTPStatus('616 Phone Number Already Exists')
 
-        token = PhoneNumberActivationTokenController(dict(phoneNumber=phone))
+        DBSession.add(Member.create_otp(phone, context.identity.id))
+        token = PhoneNumberActivationToken(dict(phoneNumber=phone))
         return dict(activationToken=token.dump())
