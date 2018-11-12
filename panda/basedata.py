70e940cea960cffe20bfe63824bbdebd78217243
shayan-7 <39003982+shayan-7@users.noreply.github.com>
2018-11-12 05:40:11 -0800

    Remove mockup and add basedata (#183)

---
+++ panda/basedata.py
@@ -0,0 +1,41 @@
+import base64
+
+from restfulpy.orm import DBSession
+
+from .models import Application, Member
+
+
+def insert(): # pragma: no cover
+    admin = Member(
+        title='admin',
+        email='admin@example.com',
+        password='123456',
+        role='admin'
+    )
+    DBSession.add(admin)
+
+    secret = 'A1dFVpz4w/qyym+HeXKWYmm6Ocj4X5ZNv1JQ7kgHBEk='
+    application = Application(
+        title='oauth',
+        redirect_uri='http://example.com/oauth',
+        secret=base64.decodebytes(bytes(secret, 'utf-8')),
+        owner=admin
+    )
+    DBSession.add(application)
+    DBSession.commit()
+
+    print('Admin has been created.')
+    print(
+        f'  Title: {admin.title}\n'
+        f'  Email: {admin.email}\n'
+        f'  Password: 123456\n'
+        f'  Role: {admin.role}\n'
+    )
+    print('Application has been created.')
+    print(
+        f'  Title: {application.title}\n'
+        f'  Secret: {secret}\n'
+        f'  Redirect uri: {application.redirect_uri}\n'
+        f'  Owner of application is User 1\n'
+    )
+
