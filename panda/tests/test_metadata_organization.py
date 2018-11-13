9a26443b6e1ff9d77332c1bfd16b373299a6a2f0
mohammad <mohammadsheikhian70@gmail.com>
2018-11-13 17:13:01 +0330

    Testing the metadata fields, closes #170

--- panda/tests/test_metadata_organization.py
+++ panda/tests/test_metadata_organization.py
@@ -15,14 +15,41 @@ class TestMember(LocalApplicationTestCase):
 
             assert status == 200
 
-            assert fields['name']['pattern'] is not None
-            assert fields['name']['pattern_description'] is not None
-            assert fields['name']['required'] is not None
-            assert fields['name']['label'] is not None
-            assert fields['name']['name'] is not None
-            assert fields['name']['example'] is not None
-            assert fields['name']['watermark'] is not None
-            assert fields['name']['minLength'] is not None
-            assert fields['name']['maxLength'] is not None
-            assert fields['name']['not_none'] is not None
+            assert fields['title']['pattern'] is not None
+            assert fields['title']['pattern_description'] is not None
+            assert fields['title']['required'] is not None
+            assert fields['title']['label'] is not None
+            assert fields['title']['name'] is not None
+            assert fields['title']['example'] is not None
+            assert fields['title']['watermark'] is not None
+            assert fields['title']['minLength'] is not None
+            assert fields['title']['maxLength'] is not None
+            assert fields['title']['not_none'] is not None
+
+            assert fields['url']['pattern'] is not None
+            assert fields['url']['pattern_description'] is not None
+            assert fields['url']['required'] is not None
+            assert fields['url']['label'] is not None
+            assert fields['url']['name'] is not None
+            assert fields['url']['example'] is not None
+            assert fields['url']['watermark'] is not None
+            assert fields['url']['minLength'] is not None
+            assert fields['url']['maxLength'] is not None
+            assert fields['url']['not_none'] is not None
+
+            assert fields['domain']['pattern'] is not None
+            assert fields['domain']['pattern_description'] is not None
+            assert fields['domain']['required'] is not None
+            assert fields['domain']['label'] is not None
+            assert fields['domain']['name'] is not None
+            assert fields['domain']['example'] is not None
+            assert fields['domain']['watermark'] is not None
+            assert fields['domain']['minLength'] is not None
+            assert fields['domain']['maxLength'] is not None
+            assert fields['domain']['not_none'] is not None
+
+            assert fields['icon']['protected'] is not None
+            assert fields['icon']['not_none'] is not None
+            assert fields['icon']['label'] is not None
+            assert fields['icon']['required'] is not None
 
