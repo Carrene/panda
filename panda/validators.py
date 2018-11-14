8154a70a0331c6a8da0796b82dcdcdcf6c27cddd
Mohammad Sheikhian <39209592+mohammadsheikhian@users.noreply.github.com>
2018-11-14 17:00:05 +0330

    The update method is implemented in the organization controller (#189)
    * Creating new validation for organization, #175
    
    * Creaing new test case for validations of organization
    
    * The update method is implemented in the organization controller, closes #175
    
    * Modifying the url pattern
    
    * The coding style is edited
    
    * The coding style is edited
    
    * The organization controller is modified

--- panda/validators.py
+++ panda/validators.py
@@ -13,6 +13,13 @@ PHONE_PATTERN = re.compile(r'^[+]{0,1}[\d+]{7,15}$')
 NAME_PATTERN = re.compile(r'^[a-zA-Z]{1}[a-z-A-Z ,.\'-]{2,19}$')
 ORGANIZATION_TITLE_PATTERN = \
     re.compile(r'^([0-9a-zA-Z]+-?[0-9a-zA-Z]*)*[\da-zA-Z]$')
+URL_PATTERN = re.compile(
+    r'^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|www.)+[a-z0-9]'
+    r'+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\.*)?$'
+)
+DOMAIN_PATTERN = re.compile(
+    r'^[^www.][a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?$'
+)
 
 
 def application_title_value_validation(title, container, field):
@@ -91,12 +98,35 @@ member_validator = validate(
 )
 
 
-organization_title_validator = validate(
+organization_create_validator = validate(
     title=dict(
         required='718 Title Not In Form',
         min_length=(1,'719 At Least 1 Character Is Needed For Title'),
         max_length=(40,'720 At Most 40 Characters Are Valid For Title'),
         pattern=(ORGANIZATION_TITLE_PATTERN, '705 Invalid Title Format'),
+    ),
+)
+
+
+organization_title_validator = validate(
+    title=dict(
+        min_length=(1,'719 At Least 1 Character Is Needed For Title'),
+        max_length=(40,'720 At Most 40 Characters Are Valid For Title'),
+        pattern=(ORGANIZATION_TITLE_PATTERN, '705 Invalid Title Format'),
+    ),
+)
+
+
+organization_domain_validator = validate(
+    domain=dict(
+        pattern=(DOMAIN_PATTERN, '726 Invalid Domain Format'),
+    ),
+)
+
+
+organization_url_validator = validate(
+    url=dict(
+        pattern=(URL_PATTERN, '725 Invalid URL Format'),
     ),
 )
 
