1bee4ac272c95e1f5576ac2e05ec4f453eaeb2a5
mohammad <mohammadsheikhian70@gmail.com>
2019-06-10 14:36:03 +0430

    Modified the name of members

--- panda/basedata.py
+++ panda/basedata.py
@@ -9,6 +9,7 @@ def insert(): # pragma: no cover
     admin = Member(
         id=1,
         title='GOD',
+        name='GOD',
         email='god@example.com',
         password='123456',
         role='admin'
@@ -28,6 +29,7 @@ def insert(): # pragma: no cover
     print('Admin has been created.')
     print(
         f'  Title: {admin.title}\n'
+        f'  Name: {admin.name}\n'
         f'  Email: {admin.email}\n'
         f'  Password: 123456\n'
         f'  Role: {admin.role}\n'
