d31aa978e0b35a6c727c455d9968bcc17b90ae2f
mohammad <mohammadsheikhian70@gmail.com>
2019-06-24 12:31:14 +0430

    The WSGI aligned with new restfulpy

--- panda_wsgi.py
+++ panda_wsgi.py
@@ -1,5 +1,12 @@
+import os
+
 from panda import panda as app
 
-app.configure()
+
+home_directory = os.environ['HOME']
+configuration_file_name=f'{home_directory}/.config/panda.yml'
+
+
+app.configure(filename=configuration_file_name)
 app.initialize_orm()
 
